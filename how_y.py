3cc882805dd058cca924a8890d687a2eed692fa3
AG-kravtsov <80669476+AG-kravtsov@users.noreply.github.com>
2021-03-15 23:56:55 +0300

    Update how_y.py
    15/03/2021 23:56

--- how_y.py
+++ how_y.py
@@ -1,17 +1,51 @@
 import datetime
+from collections import namedtuple
+
+InnerBlock = namedtuple('Дата', 'years, months, days')
 
 class Employee:
     def __init__(self):
-        self.how_long_post = None
+        # self.how_long_post = None
+        self.date_new_post = None
 
     def set_date_post(self, day, month, year):
-        cur_date = datetime.datetime.today()
-        self.how_long_post = cur_date - datetime(year, month, day)
+        self.date_new_post = datetime.datetime(year, month, day)
 
+    def get_date_post(self):
+        cur_date = datetime.datetime.today()
+        date = self.date_new_post
+
+        day, month, year = 0, 0, 0
+
+        if cur_date.day < date.day:
+            # day = date.day - cur_date.day
+            day = (cur_date - datetime.timedelta(date.day)).day
+        elif cur_date.day >= date.day:
+            if cur_date.month != date.month:
+                month += 1
+            day = cur_date.day - date.day
+        if cur_date.month <= date.month:
+            month += date.month - cur_date.month
+        elif cur_date.month > date.month and cur_date.year > date.year:
+            # year += 1
+            # print(year)
+            month += cur_date.month - date.month
+
+        year += cur_date.year - date.year
+
+        print(year, month, day)
+
+        # return InnerBlock(
+        #     years=year,
+        #     months=month,
+        #     days=day,
+        # )
 
 def main():
     empl = Employee()
-
+    empl.set_date_post(15, 4, 2020)
+    empl.get_date_post()
+    # years, month, days = empl.get_date_post()    
 
 if __name__ == '__main__':
     main()
