18b95926b0015563a21832802084858bb38dcaa6
Anton <a.kravtsov83@gmail.com>
2021-03-16 17:18:49 +0300

    Update how_y.py
    16-03-2021 17:18

--- how_y.py
+++ how_y.py
@@ -1,4 +1,5 @@
 import datetime
+from dateutil.relativedelta import relativedelta
 from collections import namedtuple
 
 InnerBlock = namedtuple('Дата', 'years, months, days')
@@ -12,40 +13,20 @@ class Employee:
         self.date_new_post = datetime.datetime(year, month, day)
 
     def get_date_post(self):
-        cur_date = datetime.datetime.today()
-        date = self.date_new_post
-
-        day, month, year = 0, 0, 0
-
-        if cur_date.day < date.day:
-            # day = date.day - cur_date.day
-            day = (cur_date - datetime.timedelta(date.day)).day
-        elif cur_date.day >= date.day:
-            if cur_date.month != date.month:
-                month += 1
-            day = cur_date.day - date.day
-        if cur_date.month <= date.month:
-            month += date.month - cur_date.month
-        elif cur_date.month > date.month and cur_date.year > date.year:
-            # year += 1
-            # print(year)
-            month += cur_date.month - date.month
-
-        year += cur_date.year - date.year
-
-        print(year, month, day)
-
-        # return InnerBlock(
-        #     years=year,
-        #     months=month,
-        #     days=day,
-        # )
+        max_date = datetime.datetime.today()
+        min_date = self.date_new_post
+        d = relativedelta(max_date, min_date)
+        return InnerBlock(
+            years=d.years,
+            months=d.months,
+            days=d.days,
+        )
 
 def main():
     empl = Employee()
-    empl.set_date_post(15, 4, 2020)
-    empl.get_date_post()
-    # years, month, days = empl.get_date_post()    
+    empl.set_date_post(15, 2, 2020)
+    d = empl.get_date_post()
+    print(d)
 
 if __name__ == '__main__':
     main()
