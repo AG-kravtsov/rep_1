213b1b8bee9633c95299213fc99b9aeb98ce4938
Anton <a.kravtsov83@gmail.com>
2021-03-17 17:33:25 +0300

    Update how_y.py
    17-03-2021 17:27

--- how_y.py
+++ how_y.py
@@ -2,31 +2,49 @@ import datetime
 from dateutil.relativedelta import relativedelta
 from collections import namedtuple
 
-InnerBlock = namedtuple('Дата', 'years, months, days')
+InnerBlock = namedtuple('Дата', 'год, месяц, день')
 
 class Employee:
     def __init__(self):
-        # self.how_long_post = None
+        self.date_today = datetime.datetime.today().date()
         self.date_new_post = None
+    
+    def set_date(self, date):###
+        year, month, day = date.rsplit('-')
+        return datetime.datetime(int(year), int(month), int(day)).date()
 
-    def set_date_post(self, day, month, year):
-        self.date_new_post = datetime.datetime(year, month, day)
-
-    def get_date_post(self):
-        max_date = datetime.datetime.today()
-        min_date = self.date_new_post
+    def set_date_post(self, cur_date, post_date):
+        if cur_date != '':
+            max_date = self.set_date(cur_date)
+        else:
+            max_date = self.date_today
+        min_date = self.set_date(post_date)
         d = relativedelta(max_date, min_date)
+
         return InnerBlock(
-            years=d.years,
-            months=d.months,
-            days=d.days,
+            год = d.years,
+            месяц = d.months,
+            день = d.days,
         )
 
 def main():
     empl = Employee()
-    empl.set_date_post(15, 2, 2020)
-    d = empl.get_date_post()
-    print(d)
+    input_curent_date = input(f'Дата когда была проверка знаний, если оставить пустым будет проставлена сегодняшняя дата {empl.date_today} ----> ')
+    input_post_date = input('Дата вступления в должность ----> ')
+    
+    date = empl.set_date_post(input_curent_date, input_post_date)
+
+    print(date)
 
 if __name__ == '__main__':
-    main()
+    print('''
+Автоматический подсчет стажа работы сотруднка в должности\n
+ДАТУ заполнять в формате: (XXXX-XX-XX) ГОД-МЕСЯЦ-ДЕНЬ 
+    ''')
+    while 1:
+        main()
+        сlose_input = input("Нажать ENTER для выхода, 1 - чтобы продолжить ----> ")
+        if сlose_input != '1':
+            break
+
+    print ("Закрытие...")
