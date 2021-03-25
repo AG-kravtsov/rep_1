a6c8e5271417362f4fd2940b4750212c7962c38a
AG-kravtsov <80669476+AG-kravtsov@users.noreply.github.com>
2021-03-25 18:45:23 +0300

    Update ot_app.py
    25-03-2021

--- ot_app.py
+++ ot_app.py
@@ -1,34 +1,64 @@
 import datetime
 # from dateutil.relativedelta import relativedelta
 
-class SaveData(Post, Employee):
-    pass
 
 class ChkKnowledge:
     pass
 
-class Briefing:
+
+class Training:
+    def __init__(self):
+        self._data = None
+        self._frequency = None
+
+    def set_data(self, new_data):
+        self._data = new_data
+
+
+class WorkSafety(Training):
     pass
+
+
+class FireSafety(Training):
+    pass
+
+
+class ElectroSafety(Training):
+    pass
+
+
+class SaveData:
+    def __init__(self):
+        self._objcts = {
+            'post': Post,
+            'employee': Employee,
+            'empl': Employee
+        }
+
+    def save(self, obj_id):
+        obj_type = self._objcts.get(obj_id)
+        print('!!!!!!!', obj_type, '!!!!!!!!!!')
+
 
 class Post:
     def __init__(self, title=None, date=None):
         # присвоить значение из Хранилища во время инициализиации
-        self.date_received = None
-        self.title = None
+        self._date_received = None
+        self._title = None
 
     def set_date_recevied(self, new_date):
-        self.date_received = new_date
+        self._date_received = new_date
 
     def set_title(self, new_title):
-        self.title = new_title
+        self._title = new_title
 
-    def save_changes(self):
-        pass
+    # def save_changes(self):
+    #     pass
 
     def get_post_info(self):
         return {
-            'date': self.date_received,
-            'title': self.title
+            'date': self._date_received,
+            'title': self._title
             }
 
 class Employee:
@@ -38,16 +68,20 @@ class Employee:
         self.surname = None
         self.second_name = None
         self.Post = Post()
-    
-    def save_changes(self):
-        pass
+
+    # def save_changes(self):
+    #     pass
 
 def main():
     empl = Employee()
     empl.Post.get_post_info()
     empl.Post.set_date_recevied('2020-01-21')
     empl.Post.set_title('spec')     # тут справочник вставляем
-    empl.Post.get_post_info()
+    e = empl.Post.get_post_info()
+    print(e)
+    wsaf = WorkSafety()
+    
+    # SaveData().save('post')
 
 if __name__ == '__main__':
     main()
